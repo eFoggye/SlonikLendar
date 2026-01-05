604b4da5ac9450e6d2c8affaf34bca69a807357e
f0ggy <egasprof@gmail.com>
2026-01-06 06:26:28 +1000

    Initial Commit

---
+++ EasyLendar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyLendar
+//
+//  Created by Egor on 06.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EasyLendarTests/EasyLendarTests.swift
@@ -0,0 +1,17 @@
+//
+//  EasyLendarTests.swift
+//  EasyLendarTests
+//
+//  Created by Egor on 06.01.2026.
+//
+
+import Testing
+@testable import EasyLendar
+
+struct EasyLendarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
